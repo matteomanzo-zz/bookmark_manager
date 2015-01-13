c66e716a998c30b78ed5fb3b983467a6f2dcbe2f
Jin Dai <jindai1783@gmail.com>
2015-01-13 16:43:02 +0000

    + some comments

--- lib/server.rb
+++ lib/server.rb
@@ -1,8 +1,13 @@
 require 'data_mapper'
 
+# is ENV defined? If yes, use directly; if no, call development.
 env = ENV['RACK_ENV'] || 'development'
 
+DataMapper::Logger.new($stdout, :debug)
+
+
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
 
 require './lib/link'
 
