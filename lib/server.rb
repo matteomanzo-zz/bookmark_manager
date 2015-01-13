84b0604915e23ac4accbbc76be2f19117b89d97c
matteomanzo <imatteo.manzo@gmail.com>
2015-01-13 16:43:35 +0000

    tested and passed basic interaction with database

--- lib/server.rb
+++ lib/server.rb
@@ -1,6 +1,9 @@
+require 'sinatra/base'
 require 'data_mapper'
 
 env = ENV['RACK_ENV'] || 'development'
+
+DataMapper::Logger.new($stdout, :debug)
 
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
@@ -9,3 +12,13 @@ require './lib/link'
 DataMapper.finalize
 
 DataMapper.auto_upgrade!
+
+
+class Bookmark < Sinatra::Base
+  get '/' do
+    'Hello Bookmark!'
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
